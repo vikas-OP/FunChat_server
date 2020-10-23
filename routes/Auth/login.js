08ebed290049db2dbf13561f159c9a011b048125
vikas-OP <67771093+vikas-OP@users.noreply.github.com>
2020-10-23 13:51:31 +0530

    active login change

--- routes/Auth/login.js
+++ routes/Auth/login.js
@@ -7,38 +7,44 @@ const { handleServerError, getUser } = require("../../common/functions")
 router.use(express.json())
 
 router.post("/", async (req, res) => {
-    try{
-       const user = await getUser(req.body.email)
-       if(user){
-            const result = await bcrypt.compare(req.body.password, user.password)
-            if(result){
-                const accessToken = generateAccessToken(user)
-                res.json({
-                    stat: "S",
-                    accessToken
-                })
-                return
-            }
-            res.json({
-                stat: "F",
-                message: "invalid password"
-            })
-            return
-       } 
-       res.json({
-           stat: "F",
-           message:"email not registered"
-       })
+  try {
+    const user = await getUser(req.body.email)
+    if (user) {
+      const result = await bcrypt.compare(req.body.password, user.password)
+      if (result) {
+        if (user.status === "inactive") {
+          res.json({
+            stat: "F",
+            message:
+              "activate your account, activation link is sent to your email",
+          })
+          return
+        }
+        const accessToken = generateAccessToken(user)
+        res.json({
+          stat: "S",
+          accessToken,
+        })
+        return
+      }
+      res.json({
+        stat: "F",
+        message: "invalid password",
+      })
+      return
     }
-    catch(err){
-        handleServerError(res)
-    }
+    res.json({
+      stat: "F",
+      message: "email not registered",
+    })
+  } catch (err) {
+    handleServerError(res)
+  }
 })
 
-function generateAccessToken(user){
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    return accessToken
+function generateAccessToken(user) {
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  return accessToken
 }
-
 
 module.exports = router
